d46dcad884194ef3313b9b416fe6d954c8d342e9
Deti Baholli <detibaholli@gmail.com>
2022-03-27 00:42:09 +0100

    signup login start

---
+++ includes/navbar.php
@@ -0,0 +1,17 @@
+<div class="navbar inline spread">
+    <div class="nav-title-icon inline">
+        <div class="icon-container center-item"><?php include './assets/logo.svg' ?></div>
+        <a href="index.php">BlogPost</a>
+    </div>
+    <div class="nav-auth flex-row">
+        <div class="auth-link-container">
+            <a href="index.php">Home</a>
+        </div>
+        <div class="auth-link-container">
+            <a href="login.php">LogIn</a>
+        </div>
+        <div class="auth-link-container">
+            <a href="signup.php">SignUp</a>
+        </div>
+    </div>
+</div>
--- login.php
+++ login.php
@@ -5,12 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./dist/App.css">
-    <link rel="stylesheet" href="./dist/index.css">
     <link rel="stylesheet" href="./dist/shared.css">
     <link rel="stylesheet" href="./dist/navbar.css">
+    <link rel="stylesheet" href="./dist/login.css">
     <title>Document</title>
 </head>
 <body>
-    <?php require './includes/navbar.php'; ?>
+    <?php require './includes/navbar.php';?>
 </body>
 </html>
