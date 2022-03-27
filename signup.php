680fdb3a6d3bd4d02655293b67440467609ac197
Deti Baholli <detibaholli@gmail.com>
2022-03-27 04:38:22 +0200

    signup

--- signup.php
+++ signup.php
@@ -5,13 +5,35 @@
 
   if ( isset( $_POST['submit'] ) ) {
 
+    print_r($_POST);
+
     $name = htmlentities( mysqli_real_escape_string( $conn, $_POST['name'] ) );
     $surname = htmlentities( mysqli_real_escape_string( $conn, $_POST['surname'] ) );
+    $username = htmlentities( mysqli_real_escape_string( $conn, $_POST['username'] ) );
     $email = htmlentities( mysqli_real_escape_string( $conn, $_POST['email'] ) );
     $password = htmlentities( mysqli_real_escape_string( $conn, $_POST['password'] ) );
     $gender = htmlentities( mysqli_real_escape_string( $conn, $_POST['gender'] ) );
     $dob = htmlentities( mysqli_real_escape_string( $conn, $_POST['dob'] ) );
 
+    if (
+      trim( $name ) != '' &&
+      trim( $surname ) != '' &&
+      trim( $username ) != '' &&
+      trim( $email ) != '' &&
+      trim( $password ) != '' &&
+      trim( $gender ) != '' &&
+      trim( $dob ) != ''
+    ) {
+        $query = "
+        insert into users
+            (Name, Surname, Username, Email, Password, Gender, DOB)
+                values ('$name','$surname', '$username', '$email', '$password', '$gender', '$dob')
+        ";
+
+        mysqli_query($conn, $query);
+    } else {
+      $showErrorMessage = true;
+    }
   }
 ?>
 
@@ -35,7 +57,7 @@
     <h1 class="signup-title">Sign Up</h1>
     <div class="signup-container">
         <form method="POST" action="<?php echo $_SERVER['PHP_SELF'] ?>">
-            <?php if ( !$showErrorMessage ): ?>
+            <?php if ( $showErrorMessage ): ?>
             <div class="error-box">
                 <strong>
                     <p>Please fill all the fields.</p>
@@ -51,6 +73,10 @@
                 <input type="text" name="surname" id="surname">
             </div>
             <div class="signup-form-field">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username">
+            </div>
+            <div class="signup-form-field">
                 <label for="email">Email</label>
                 <input type="email" name="email" id="email">
             </div>
@@ -59,8 +85,8 @@
                 <input type="password" name="password" id="password">
             </div>
             <div class="signup-form-field">
-                <label for="">Gender</label>
-                <select name="gender">
+                <label for="gender">Gender</label>
+                <select name="gender" id="gender">
                     <option value="" selected disabled>Select</option>
                     <option value="male">Male</option>
                     <option value="female">Female</option>
