b4fd3de301f9d7ef028cda5815695ac7cfeb3806
Blendi Rrustemi <blendirrustemi9@gmail.com>
2022-03-27 01:45:31 +0100

    Login.php and Login.css

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,20 +11,30 @@
     <link rel="stylesheet" href="./dist/login.css">
     <title>Login</title>
 </head>
-<body>
-    <?php require './includes/navbar.php';?>
 
+<body>
+    <?php require './includes/navbar.php'; ?>
+
+    <h1 class="login_title">Log In</h1>
 
     <div class="login-container">
-        <form action="">
-            <div class="login-form-field">
-                <label for="">Username:</label>
-                <input type="text" name="username_login">
-                <label for="">Password:</label>
-                <input type="password" name="password_login">
 
+        <form action="">
+            
+            <div class="login-form-field username_field">
+                <label for="">Username</label>
+                <input type="text" name="username_login">
             </div>
+
+            <div class="login-form-field password_field">
+                <label for="">Password</label>
+                <input type="password" name="password_login">
+            </div>
+
+            <input type="submit" class="login_button" value="Log In">
+
         </form>
     </div>
 </body>
+
 </html>
