7ec178238fc3a055051614889a6d0e8f50f54e2e
Deti Baholli <detibaholli@gmail.com>
2022-03-27 03:32:54 +0200

    database

---
+++ includes/database.php
@@ -0,0 +1,9 @@
+<?php
+
+    $conn = mysqli_connect( 'localhost', 'root', '', 'full_stack_project' );
+
+    if ( mysqli_connect_errno() ) {
+    echo "Failed to connect to MySQL" . mysqli_connect_errno();
+    }
+
+?>
